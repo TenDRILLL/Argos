10dee12fbc3ae264b86719c310f0fc353fdd4ac7
TenDRILLL <sofyeer@gmail.com>
2022-11-23 19:35:30 +0200

    enum

---
+++ src/enums/memberType.ts
@@ -0,0 +1,9 @@
+const memberType = Object.freeze({
+    0: Symbol("None"),
+    1: Symbol("Beginner"),
+    2: Symbol("Member"),
+    3: Symbol("Admin"),
+    4: Symbol("ActingFounder"),
+    5: Symbol("Founder")
+});
+export default memberType;
---
+++ src/enums/membershipType.ts
@@ -0,0 +1,11 @@
+const membershipType = Object.freeze({
+    1: Symbol("Xbox"),
+    2: Symbol("Playstation"),
+    3: Symbol("Steam"),
+    4: Symbol("Blizzard"),
+    5: Symbol("Stadia"),
+    10: Symbol("TigerDemon"),
+    254: Symbol("Bungie.net"),
+    "-1": Symbol("All")
+});
+export default membershipType;
