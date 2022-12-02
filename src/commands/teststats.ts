279e1a923c238a1b9e9bb71e70e87a4814e2cabe
TenDRILLL <sofyeer@gmail.com>
2022-12-02 23:21:35 +0200

    reuse embed for teststats

--- src/commands/teststats.ts
+++ src/commands/teststats.ts
@@ -26,9 +26,19 @@ export default class TestStats extends Command {
         }
         const bungoName = await d2client.getBungieName(dbUser.bungieId);
         interaction.editReply({
-            content: `${bungoName}'s logged stats:
-Light: ${dbUser.stats.light}
-KD: ${dbUser.stats.kd}`,
+            embeds: [{
+                "type": "rich",
+                "title": "A new clan request",
+                "color": 0xae27ff,
+                "fields": [
+                    {"name": "User", "value": `${bungoName}`, "inline": true},
+                    {"name": "Power Level", "value": `${dbUser.stats?.light ?? "UNKNOWN"}`, "inline": true},
+                    {"name": "Raid", "value": `${dbUser.raids?.Total ?? "UNKNOWN"}`, "inline": true},
+                    {"name": "Dungeon", "value": `${dbUser.dungeons?.Total ?? "UNKNOWN"}`, "inline": true},
+                    {"name": "Grandmaster", "value": `${dbUser.grandmasters?.Total ?? "UNKNOWN"}`, "inline": true},
+                    {"name": "PvP K/D", "value": `${Math.round((dbUser.stats?.kd ?? 0) * 100)/100}`}
+                ]
+            }],
             components: [{type: 1, components: [{type: 2, label: "Delete", style: 4, custom_id: `delete-${authorID}`}]}]
         }).catch(e => console.log(e));
     }
