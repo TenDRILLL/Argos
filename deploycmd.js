9180a3453b210e05096656f19a4e7d8e58cfad9b
TenDRILLL <sofyeer@gmail.com>
2022-12-04 08:26:11 +0200

    add d2stats command to argos

--- deploycmd.js
+++ deploycmd.js
@@ -39,6 +39,60 @@ const commands = [
     }, {
         name: "registrationlink",
         description: "Send registration link."
+    }, {
+        name: "d2stats",
+        description: "Get Destiny 2 statistics of yourself or the requested user.",
+        options: [
+            {
+                type: 1,
+                name: "summary",
+                description: "Requested user's general statistics Argos monitors.",
+                options: [
+                    {
+                        type: 6,
+                        name: "user",
+                        description: "The Discord user whose stats you wish to request.",
+                        required: false
+                    }
+                ]
+            }, {
+                type: 1,
+                name: "raids",
+                description: "Requested user's raid completions per raid.",
+                options: [
+                    {
+                        type: 6,
+                        name: "user",
+                        description: "The Discord user whose stats you wish to request.",
+                        required: false
+                    }
+                ]
+            }, {
+                type: 1,
+                name: "dungeons",
+                description: "Requested user's dungeon completions per dungeon.",
+                options: [
+                    {
+                        type: 6,
+                        name: "user",
+                        description: "The Discord user whose stats you wish to request.",
+                        required: false
+                    }
+                ]
+            }, {
+                type: 1,
+                name: "grandmasters",
+                description: "Requested user's Grandmaster Nightfall completions per Grandmaster Nightfall.",
+                options: [
+                    {
+                        type: 6,
+                        name: "user",
+                        description: "The Discord user whose stats you wish to request.",
+                        required: false
+                    }
+                ]
+            }
+        ]
     }
 ];
 
