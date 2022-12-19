e6204a12388a4c88bc0322c7a335622de2a6ae7b
Ugi <javest@utu.fi>
2022-12-19 21:32:53 +0200

    Finished xur command and made some changes if weapon perks are ever needed

--- src/commands/xur.ts
+++ src/commands/xur.ts
@@ -10,46 +10,36 @@ export default class Xur extends Command {
     }
 
     async cmdRun(interaction, d2client){
+        await interaction.defer();
         d2client.refreshToken(d2client.adminuserID).then(q => {
-                d2client.apiRequest("getDestinyCharacters", {
-                    membershipType: 3,
-                    destinyMembershipId: d2client.DB.get(d2client.adminuserID).destinyId}).then(t => {
-                        const resp = t.Response as CharacterQuery;
-                        d2client.apiRequest("getVendorInformation", {
-                            membershipType: 3,
-                            destinyMembershipId: d2client.DB.get(d2client.adminuserID).destinyId,
-                            characterId: resp.characters[0].characterId.toString(),
-                            vendorHash: "2190858386" /*xur id*/},
-                            {"Authorization": `Bearer ${q.tokens.accessToken}`}
-                        ).then(d => {
-                            const info = d.Response as vendorQuery;
-                            const location = info.vendor.data.vendorLocationIndex;
-                            interaction.reply({
-                                content: "Loading...",
-                                flags: 64
-                            })
-                            this.generateEmbed(info.sales.data, d2client, location).then(embed => {
-                                interaction.newMessage({
-                                    embeds: embed
-                                }).catch(e => console.log(e));
-                            })
-                        }).catch(e => {
-                            console.log(`Xur isn't anywhere / something went wrong ${e}`)
-                            interaction.newMessage({
-                                content: e.toString(),
+            d2client.apiRequest("getDestinyCharacters", {
+                membershipType: 3,
+                destinyMembershipId: d2client.DB.get(d2client.adminuserID).destinyId}).then(t => {
+                    const resp = t.Response as CharacterQuery;
+                    d2client.apiRequest("getVendorInformation", {
+                        membershipType: 3,
+                        destinyMembershipId: d2client.DB.get(d2client.adminuserID).destinyId,
+                        characterId: resp.characters.filter(character => character.deleted === false)[0].characterId,
+                        vendorHash: "2190858386" /*xur id*/},
+                        {"Authorization": `Bearer ${q.tokens.accessToken}`}
+                    ).then(d => {
+                        const info = d.Response as vendorQuery;
+                        const location = info.vendor.data.vendorLocationIndex;
+                        this.generateEmbed(info.sales.data, d2client, location).then(embed => {
+                            interaction.editReply({
+                                embeds: [embed],
                                 flags: 64
                             }).catch(e => console.log(e));
-                            /*interaction.newMessage({
-                                embeds: [
-                                    {
-                                    "title": `Xur doesn't seem to be on any planet.`,
-                                    "color": 0xAE27FF
-                                    }
-                                ]
-                            }).catch(e => console.log(e));
-                            */
-                        });
-                })
+                        })
+                    }).catch(e => {
+                        console.log(`Xur isn't anywhere / something went wrong ${e}`)
+                        interaction.editReply({
+                            content: `Xur doesn't seem to be on any planet, or perhaps something went wrong in searching for him,
+${e.toString()}`,
+                            flags: 64
+                        }).catch(e => console.log(e));
+                    });
+            })
         }).catch(() => console.log("Admin user not in DB"));
     }
     private generateEmbed(components: vendorSaleComponent[], d2client, locationIndex) {
@@ -63,14 +53,27 @@ export default class Xur extends Command {
             )})
         return Promise.all(promises).then(data => {
             const xurLocations = ["Hangar, The Tower", "Winding Cove, EDZ", "Watcher’s Grave, Nessus"];
-            const embed = [{
+            const embed = {
                 "title": `Xûr is at ${xurLocations[locationIndex]}`,
                 "color": 0xAE27FF,
                 "description": "He is currently selling the following exotics",
-                "fiels": data.filter(entity => entity.inventory.tierTypeName === "Exotic" && entity.displayProperties.name != "Exotic Engram").map(e => 
-                    ({"name": e.displayProperties.name, "value": "\u200B", "inline": true}))
-            }]
+                "fields": this.generateFields(data.filter(entity => entity.inventory.tierTypeName === "Exotic" && !["Exotic Engram","Xenology"].includes(entity.displayProperties.name)),3)
+            }
             return embed;
         })
     };
+    generateFields(exotics,number) {
+        let rows: object[] = [];
+        for (let i = 0; i < number; i++) {
+            rows.push({"name": "\u200B", "value": "", "inline": true})
+        }
+        for (var i = 0; i < exotics.length; i++) {
+            const exotic = exotics[i];
+            rows[i % number]["value"] += `**${exotic.displayProperties.name}**
+${i < exotics.length-number ? exotic.itemTypeAndTierDisplayName+"\n\u200b" : exotic.itemTypeAndTierDisplayName}
+
+`
+                }
+        return rows;
+    }
 }
