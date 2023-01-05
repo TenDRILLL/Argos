3ac331d97e3999d641cf6e08b8cb090ee0239bf7
TenDRILLL <sofyeer@gmail.com>
2023-01-05 22:53:08 +0200

    typing for discordUser

--- src/props/dbUser.ts
+++ src/props/dbUser.ts
@@ -20,6 +20,21 @@ export class DBUser {
         scope: string;
         tokenType: string;
     };
+    discordUser: {
+        id: string;
+        username: string;
+        avatar: string;
+        avatar_decoration: string;
+        discriminator: string;
+        public_flags: number;
+        flags: number;
+        banner: string;
+        banner_color: string;
+        accent_color: number;
+        locale: string;
+        mfa_enabled: boolean;
+        premium_type: number;
+    }
 }
 
 export class ActivityObject {
