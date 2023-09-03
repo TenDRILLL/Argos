1aa5846f05755ec9ab85a2543fd27c21946b9275
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2023-09-04 01:46:38 +0300

    add CE manually for lfg for now

--- src/commands/lfg.ts
+++ src/commands/lfg.ts
@@ -353,7 +353,7 @@ export default class LFG extends Command {
                 let activities;
                 switch(option.options[0].value){
                     case "Raid":
-                        activities = [];
+                        activities = ["Crota's End"];
                         let sunsetRaids = ["Leviathan", "Leviathan, Eater of Worlds", "Leviathan, Spire of Stars", "Scourge of the Past", "Crown of Sorrow"];
                         for (let [key, data] of d2client.activityIdentifierDB) {
                             if(data.type === 0 && !(sunsetRaids.includes(key))){
